ced2cebbd72cf170f4891171dee10986472d1142
Mari Hara <haramari.rps@gmail.com>
2019-05-11 00:36:03 +0900

    モデルのリレーションを定義

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  public function Comments(){
+    //1つの投稿は複数のコメントを所有する
+    return $this->hasMany('App\Comment');
+  }
+
+  public function Category(){
+    //1つの投稿は1つのカテゴリーに所属する
+    return $this->belingsTo('App\Category');
+  }
 }
